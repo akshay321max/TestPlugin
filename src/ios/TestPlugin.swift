fc6d47df0d60744186c6939b1be7681a2a9dbbbe
akshay321max <akshay321max@gmail.com>
2025-08-05 22:50:05 +0530

    Update TestPlugin.swift

--- src/ios/TestPlugin.swift
+++ src/ios/TestPlugin.swift
@@ -1,7 +1,7 @@
 import Foundation
 import Cordova
 
-@objc(Logger) class TestPlugin: CDVPlugin {
+@objc(TestPlugin) class TestPlugin: CDVPlugin {
     @objc(log:)
     func log(command: CDVInvokedUrlCommand) {
         let message = command.argument(at: 0) as? String ?? "No message"
